65980cea3818f1135b0f10fb4b3a2b87a8c7eee2
Oleksandr Minenko <OMinenko@luxoft.com>
2018-08-31 09:46:37 +0300

    uptate htm

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ git clone https://github.com/Tencent/rapidjson.git
 https://github.com/jeromeetienne/virtualjoystick.js.git
 
 http://rapidjson.org/md_doc_dom.html
+
+http://dmitrybaranovskiy.github.io/raphael/
+http://rangeslider.js.org/
--- frontend/rcbrowser.js
+++ frontend/rcbrowser.js
@@ -1,4 +1,13 @@
 xmlHttp=createXmlHttpObject();
+var range_rotation;
+var range_steering_wheel;
+var range_wheel_L0;
+var range_wheel_L1;
+var	range_wheel_R0;
+var range_wheel_R1;
+var range_diff_L;
+var range_diff_R;
+var range_power;
 
 function createXmlHttpObject(){
     if(window.XMLHttpRequest){
@@ -9,11 +18,86 @@ function createXmlHttpObject(){
     return xmlHttp;
   }
 
+function update_rotate(update_rotate){
+	console.log('update_rotate='+update_rotate);
+}
+
+function on_stop(){
+	console.log('on_stop');
+	range_rotation.value=range_rotation.defaultValue;
+	range_steering_wheel.value=range_steering_wheel.defaultValue;
+	range_wheel_L0.value=range_wheel_L0.defaultValue;
+	range_wheel_L1.value=range_wheel_L1.defaultValue;
+	range_wheel_R0.value=range_wheel_R0.defaultValue;
+	range_wheel_R1.value=range_wheel_R1.defaultValue;
+	range_diff_L.value=range_diff_L.defaultValue;
+	range_diff_R.value=range_diff_R.defaultValue;
+	range_power.value=range_power.defaultValue;
+	range_wheels_xx_on_input();
+}
+
+
+function update_rotation(){
+	console.log('update_rotation='+range_rotation.value);
+	range_wheel_L1.value=range_wheel_L0.value=range_rotation.value;	
+	range_wheel_R1.value=range_wheel_R0.value=range_rotation.value*(-1);
+	range_wheels_xx_on_input();
+}
+
+
+function update_move(){
+	console.log('update_move'
+			+'steering_wheel='+range_steering_wheel.value
+			+' power='+range_power.value
+			+' diff L:R='+range_diff_L.value+':'+range_diff_R.value			
+	);	
+	var PL=range_power.value*range_steering_wheel.value/100
+	var PR=range_power.value-PL;
+	
+	var PL0=PL*range_diff_L.value/100;	
+	var PR0=PR*range_diff_R.value/100;
+	range_wheel_L0.value=PL0;
+	range_wheel_L1.value=PL-PL0;
+	range_wheel_R0.value=PR0;
+	range_wheel_R1.value=PR-PR0;
+	
+	range_wheels_xx_on_input();
+}
+
+function range_wheels_xx_on_input(){
+	set_wheels(range_wheel_L0.value,range_wheel_L1.value,
+			range_wheel_R0.value,range_wheel_R1.value);
+}
+function set_wheels(l0,l1,r0,r1){
+	console.log('range_wheel_L0:1='+l0+':'+l1+ ' R0:1='+r0+':'+r1);	
+}
+
 function init(){
 	console.log('started');
-	var container_=document.getElementById('container');
+	range_rotation=document.getElementById('rotation');
+	range_wheel_L0=document.getElementById('wheel_L0');
+	range_wheel_L1=document.getElementById('wheel_L1');
+	range_wheel_R0=document.getElementById('wheel_R0');
+	range_wheel_R1=document.getElementById('wheel_R1');
+	range_steering_wheel=document.getElementById('steering_wheel');
+	range_diff_L=document.getElementById('diff_L');
+	range_diff_R=document.getElementById('diff_R');
+	range_power=document.getElementById('power');	
+		
+	range_rotation.addEventListener("input", update_rotation);
+	range_steering_wheel.addEventListener("input", update_move);
+	range_diff_L.addEventListener("input", update_move);
+	range_diff_R.addEventListener("input", update_move);
+	range_power.addEventListener("input", update_move);
+	range_wheel_L0.addEventListener("input", range_wheels_xx_on_input);
+	range_wheel_L1.addEventListener("input", range_wheels_xx_on_input);
+	range_wheel_R0.addEventListener("input", range_wheels_xx_on_input);
+	range_wheel_R1.addEventListener("input", range_wheels_xx_on_input);
+	
+	
+	var container_=document.getElementById('fpv');
 	var joystick	= new VirtualJoystick({
-		container	: document.getElementById('container'),
+		container	: document.getElementById('fpv'),
 		mouseSupport	: true,
 	});
 	let dX;
